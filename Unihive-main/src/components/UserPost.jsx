0ba530a13b8ec8ffbe0490fd36f8fb5439961ada
rimon2002 <raihanrimon854@gmail.com>
2025-04-15 21:31:50 +0600

    added all frontend

--- Unihive-main/src/components/UserPost.jsx
+++ Unihive-main/src/components/UserPost.jsx
@@ -1,92 +1,91 @@
-import { Avatar } from "@chakra-ui/avatar";
-import { Image } from "@chakra-ui/image";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Text, Image } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import Actions from "./Actions";
 import { useState } from "react";
 
 const UserPost = ({ postImg, postTitle, likes, replies }) => {
   const [liked, setLiked] = useState(false);
-
   return (
-    <Flex gap={3} mb={4} py={5}>
-      <Flex flexDirection={"column"} alignItems={"center"}>
-        <Avatar size="md" name="Mark Zuckerberg" src="/Rimon.jpg" />
-        <Box w="1px" h={"full"} bg="gray.light" my={2}></Box>
-        <Box position={"relative"} w={"full"}>
-          <Avatar
-            size="xs"
-            name="John doe"
-            src="https://bit.ly/dan-abramov"
-            position={"absolute"}
-            top={"0px"}
-            left="15px"
-            padding={"2px"}
-          />
-          <Avatar
-            size="xs"
-            name="John doe"
-            src="https://bit.ly/sage-adebayo"
-            position={"absolute"}
-            bottom={"0px"}
-            right="-5px"
-            padding={"2px"}
-          />
-          <Avatar
-            size="xs"
-            name="John doe"
-            src="https://bit.ly/prosper-baba"
-            position={"absolute"}
-            bottom={"0px"}
-            left="4px"
-            padding={"2px"}
-          />
-        </Box>
-      </Flex>
-      <Flex flex={1} flexDirection={"column"} gap={2}>
-        <Flex justifyContent={"space-between"} w={"full"}>
-          <Flex w={"full"} alignItems={"center"}>
-            <Text fontSize={"sm"} fontWeight={"bold"}>
-              DevRimon
-            </Text>
-            <Image src="/Chest-logo-01.png" w={4} h={4} ml={1} />
-          </Flex>
-          <Flex gap={4} alignItems={"center"}>
-            <Text fontStyle={"sm"} color={"gray.light"}>
-              1d
-            </Text>
-            <BsThreeDots />
-          </Flex>
-        </Flex>
-
-        <Text fontSize={"sm"}>{postTitle}</Text>
-
-        {postImg && (
-          <Box
-            borderRadius={6}
-            overflow={"hidden"}
-            border={"1px solid"}
-            borderColor={"gray.light"}
-          >
-            <Image src={postImg} w={"full"} />
+    <Link to={"/rimon/post/1"}>
+      <Flex gap={3} mb={4} py={5}>
+        <Flex flexDirection={"column"} alignItems={"center"}>
+          <Avatar size="md" name="lucifer" src="/post2.jpg" />
+          <Box h={"full"} bg="gray.light" my={2}></Box>
+          <Box position={"relative"} w={"full"}>
+            <Avatar
+              size="xs"
+              name="John doe"
+              src="https://bit.ly/dan-abramov"
+              position={"absolute"}
+              top={"0px"}
+              left="15px"
+              padding={"2px"}
+            />
+            <Avatar
+              size="xs"
+              name="John doe"
+              src="https://bit.ly/sage-adebayo"
+              position={"absolute"}
+              bottom={"0px"}
+              right="-5px"
+              padding={"2px"}
+            />
+            <Avatar
+              size="xs"
+              name="John doe"
+              src="https://bit.ly/prosper-baba"
+              position={"absolute"}
+              bottom={"0px"}
+              left="4px"
+              padding={"2px"}
+            />
           </Box>
-        )}
-
-        <Flex gap={3} my={1}>
-          <Actions liked={liked} setLiked={setLiked} />
         </Flex>
+        <Flex flex={1} flexDirection={"column"} gap={2}>
+          <Flex justifyContent={"space-between"} w={"full"}>
+            <Flex w={"full"} alignItems={"center"}>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+                Rimon
+              </Text>
+              <Image src="/CSElogoOnly.png " w={4} h={4} ml={1} />
+            </Flex>
+            <Flex gap={4} alignItems={"center"}>
+              <Text fontStyle={"sm"} color={"gray.light"}>
+                1d
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
 
-        <Flex gap={2} alignItems={"center"}>
-          <Text color={"gray.light"} fontSize="sm">
-            {replies} replies
-          </Text>
-          <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-          <Text color={"gray.light"} fontSize="sm">
-            {likes} likes
-          </Text>
+          <Text fontSize={"sm"}>{postTitle}</Text>
+          {postImg && (
+            <Box
+              borderRadius={9}
+              overflow={"hidden"}
+              border={"1px solid"}
+              borderColor={"gray.light"}
+            >
+              <Image src={postImg} w={"full"} />
+            </Box>
+          )}
+
+          <Flex gap={3} my={1}>
+            <Actions liked={liked} setLiked={setLiked} />
+          </Flex>
+
+          <Flex gap={2} alignItems={"center"}>
+            <Text color={"gray.light"} fontSize="sm">
+              {replies} replies
+            </Text>
+            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+            <Text color={"gray.light"} fontSize="sm">
+              {likes} likes
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Link>
   );
 };
 
--- Unihive-main/src/pages/PostPage.jsx
+++ Unihive-main/src/pages/PostPage.jsx
@@ -1,7 +1,16 @@
-import { Avatar, Flex, Image, Text, Box } from "@chakra-ui/react";
+import {
+  Avatar,
+  Flex,
+  Image,
+  Text,
+  Box,
+  Divider,
+  Button,
+} from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
 import Actions from "../components/Actions";
 import { useState } from "react";
+import Comment from "../components/Comment";
 
 const PostPage = () => {
   const [liked, setLiked] = useState(false);
@@ -9,45 +18,85 @@ const PostPage = () => {
   return (
     <>
       <Flex>
-        <Flex w={"full"} alignItems={"center"} gap={3}>
-          <Avatar src="/post1.JPG" size={"md"} name="raihan rimon" />
+        <Flex w="full" alignItems="center" gap={3}>
+          <Avatar src="/post1.JPG" size="md" name="raihan rimon" />
 
           <Flex>
-            <Text fontsize={"sm"} fontWeight={"bold"}>
+            <Text fontSize="sm" fontWeight="bold">
               raihanrimon
             </Text>
-            <Image src="/Chest-logo-01.png" w={"4"} h={"4"} ml={"4"} />
+            <Image src="/Chest-logo-01.png" w={4} h={4} ml={4} />
           </Flex>
         </Flex>
-        <Flex gap={4} alignItems={"center"}>
-          <Text fontSize={"sm"} color={"gray.light"}>
+        <Flex gap={4} alignItems="center">
+          <Text fontSize="sm" color="gray.light">
             1d
           </Text>
           <BsThreeDots />
         </Flex>
       </Flex>
+
       <Text my={3}>This is our First University Project</Text>
+
       <Box
         borderRadius={6}
-        overflow={"hidden"}
-        border={"1px solid"}
-        borderColor={"gray.light"}
+        overflow="hidden"
+        border="1px solid"
+        borderColor="gray.light"
       >
-        <Image src={"/iron.png"} w={"full"} />
+        <Image src="/iron.png" w="full" />
       </Box>
+
       <Flex gap={3} my={3}>
         <Actions liked={liked} setLiked={setLiked} />
       </Flex>
 
-      <Flex gap={2} alignItems={"center"}>
-        <Text color={"gray.light"} fontSize={"sm"}>
+      <Flex gap={2} alignItems="center">
+        <Text color="gray.light" fontSize="sm">
           2768 replies
         </Text>
-        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-        <Text color={"gray.light"} fontSize={"sm"}>
-          5278 likes
+        <Box w={0.5} h={0.5} borderRadius="full" bg="gray.light"></Box>
+        <Text color="gray.light" fontSize="sm">
+          {2000 + (liked ? 1 : 0)} likes
         </Text>
       </Flex>
+
+      <Divider my={4} />
+
+      <Flex justifyContent="space-between">
+        <Flex gap={2} alignItems="center">
+          <Text fontSize="2xl">👍</Text>
+          <Text color="gray.light">
+            In this website you connect with our alumni, friends and you can
+            post, comment and chat with each other.
+          </Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+
+      <Divider my={4} />
+
+      <Comment
+        comment="Looks good"
+        createdAt="2d"
+        likes={43}
+        username="johndoe"
+        userAvatar="https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd"
+      />
+      <Comment
+        comment="Looks good"
+        createdAt="2d"
+        likes={43}
+        username="johndoe"
+        userAvatar="https://cdn.myanimelist.net/r/84x124/images/characters/9/105421.webp?s=269ff1b2bb9abe3ac1bc443d3a76e863"
+      />
+      <Comment
+        comment="Looks good"
+        createdAt="2d"
+        likes={43}
+        username="johndoe"
+        userAvatar="https://cdn.myanimelist.net/r/84x124/images/characters/7/284129.webp?s=a8998bf668767de58b33740886ca571c"
+      />
     </>
   );
 };
